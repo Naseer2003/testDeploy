9762262bc97e56cdc5fb119acf090dff67365755
Naseer.K <naseerkovvuru2003@gmail.com>
2025-08-22 15:03:12 +0530

    prepared for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import connection from "./config/db.js";
 import userRouter from "./routes/user.routes.js";
 import adminRouter from "./routes/admin.routes.js";
@@ -9,6 +10,7 @@ import fileRouter from "./routes/file.routes.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -24,19 +26,27 @@ if (
   process.exit(1);
 }
 
-app.use(
-  cors({
+if(process.env.NODE_ENV !== "production") {
+  app.use(cors({
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-  }),
-);
+  }));
+}
+
 
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/folder", folderRouter);
 app.use("/api/files", fileRouter);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 connection().then(() => {
   app.listen(PORT, () => {
--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,8 +1,10 @@
 // src/services/api.ts
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api"
+
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: BASE_URL,
 });
 
 export const fetchFolders = () =>
