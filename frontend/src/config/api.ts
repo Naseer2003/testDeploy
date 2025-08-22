acbc19d4546fe9a61e9a1f1ccc17553639d99252
Naseer.K <naseerkovvuru2003@gmail.com>
2025-08-22 15:23:01 +0530

    deployment

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,11 +1,14 @@
-// src/services/api.ts
+
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api"
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : "https://filecloud-zadq.onrender.com/api"; // your Render backend
 
 export const api = axios.create({
   baseURL: BASE_URL,
 });
 
 export const fetchFolders = () =>
-  api.get("/folder/get_folders").then((res) => res.data); // [{ _id: 'Brochures', totalItems: 7 }, ...]
+  api.get("/folder/get_folders").then((res) => res.data);
